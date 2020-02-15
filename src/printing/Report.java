bfed4a0fd8e05b6fa24824dca317f236b1a665f6
Mihail Dorofeev <crabgore.gonna.hate@gmail.com>
2020-02-15 14:17:31 +0200

    Initial commit

---
+++ src/printing/Report.java
@@ -0,0 +1,20 @@
+package printing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Report{
+    private List<ReportItem> items; // Отчетные данные
+
+    // расчет отчетных данных
+    public void calculate(){
+        items =  new ArrayList<ReportItem>();
+        items.add(new ReportItem("First", (float)5));
+        items.add(new ReportItem("Second", (float)6));
+    }
+
+    public void output(){
+        PrintReport reportPrint = new PrintReport();
+        reportPrint.output(items);
+    }
+}
